faeb8a491a61f276b86b21d13ccc40c3bfedec15
Jonathan <jonathan.videira@procempa.com.br>
2016-12-29 15:19:14 -0200

    Habilitando retorno em JSON dos controllers

--- src/main/java/br/com/ajvideira/cursos/alura/springmvc/configuration/AppWebConfiguration.java
+++ src/main/java/br/com/ajvideira/cursos/alura/springmvc/configuration/AppWebConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.ajvideira.cursos.alura.springmvc.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
@@ -12,12 +15,15 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
@@ -85,6 +91,19 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
         factory.setConfigLocation(new ClassPathResource("ehcache.xml"));
         factory.setShared(true);
         return factory;
+    }
+    
+    @Bean
+    public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
+    	List<ViewResolver> viewResolvers = new ArrayList<>();
+    	viewResolvers.add(internalResourceViewResolver());
+    	viewResolvers.add(new JsonViewResolver());
+    	
+    	ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+    	resolver.setContentNegotiationManager(manager);
+    	resolver.setViewResolvers(viewResolvers);
+    	
+    	return resolver;
     }
 
 }
